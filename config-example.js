53cd90dcda1170f11593e621b64e6a928557569f
thunder <thunderace@free.fr>
2018-03-15 17:30:00 +0100

    adaptation

---
+++ config-example.js
@@ -0,0 +1,7 @@
+var MQTTbroker = 'www.xxxx.fr';
+var MQTTport = 9001;
+var MQTTsubTopic = '/home_dev/nodes/teleinfo/sensors/RTPAPP/value'; //works with wildcard # and + topics dynamically now
+var MQTTUser = 'xxxxxxxx';
+var MQTTPassword = 'xxxxxx';
+var MQTTUseSSL = true;
+
